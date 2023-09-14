b7d3f434bb4b26be6c318611bba2571a859d262e
tomas-valcarcel <tomasvalcarcel2212@gmail.com>
2023-09-14 11:37:29 -0300

    actualizacion del trabajo

--- index.js
+++ index.js
@@ -1,35 +1,43 @@
-class Super {
-    constructor(nombreDelProducto,precioDelProducto) {
-      this.nombreDelProducto = nombreDelProducto
-      this.precioDelProducto = precioDelProducto
+const labelCompra = document.querySelector('#labelCompra');
+const inputNombre = document.querySelector('#inputNombre');
+const inputPrecio = document.querySelector('#inputPrecio');
+const divListaDeProductos = document.querySelector('#listaDeProductos')
+const seccionGraciasPorTuCompra = document.querySelector('#seccionGraciasPorTuCompra')
+
+const productos = JSON.parse(localStorage.getItem('productos')) || [];
+
+mostrarGraciasPorTuCompra();
+class Producto {
+  constructor({ nombre, precio }) {
+    this.nombre = nombre;
+    this.precio = precio;
+  }
+}
+
+labelCompra.onsubmit = e => {
+  e.preventDefault();
+
+  const nombre = inputNombre.value;
+  const precio = inputPrecio.value;
+
+  const prod = new Producto({ nombre, precio });
+
+  guardarProd(prod);
+}
+
+function guardarProd(prod) {
+  productos.push(prod);
+  localStorage.setItem('productos', JSON.stringify(productos));
+  mostrarGraciasPorTuCompra();
+}
+
+function mostrarGraciasPorTuCompra(){
+  if (productos.length > 0) {
+    seccionGraciasPorTuCompra.style.display = 'block';
+    let listaProductoHtml = '<ul>';
+    for (const prod of productos) {
+      listaProductoHtml += `<li>${prod.nombre}</a></li>`;
     }
+    divListaDeProductos.innerHTML = listaProductoHtml + '</ul>';
   }
-  
-
-
-  function procesoDeCompraDelProducto() {
-    const nombreDelProducto = prompt('Ingrese el nombre del producto seleccionado')
-    const precioDelProducto = parseFloat(prompt('Ingrese el precio del producto'))
-    return new Super(nombreDelProducto,precioDelProducto)
-  }
-
-  const Productos = []
-     
-  let continuaComprandoProductos = true
-  while (continuaComprandoProductos) {
-    const prod = procesoDeCompraDelProducto()
-  
-    Productos.push(prod)
-  
-    const respuesta = prompt('Quiere comprar algun item mas? si/no')
-    if (respuesta === 'no') {
-        continuaComprandoProductos = false
-    }
-  }
-
-
-  let total = 0
-  const precio = Productos.map(p => p.precioDelProducto)
-  precio.forEach(p => { total = total + p })
-  
-  alert('Precio total de su compra' + ' '  +   total)
+}
